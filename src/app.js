21d22f8322a2ec43835d3309eb9039c1d694287e
g-Ball <georgiballabanov@gmail.com>
2022-09-14 22:09:10 +0300

    Added basic routing.

--- src/app.js
+++ src/app.js
@@ -1,2 +1,23 @@
-import {html,render} from '.'
+// import {html,render} from '.'
 // import {html,render} from './lib/lit-html.js'
+// import page from '../node_modules/page'
+
+import { aboutPage } from './about.js'
+import { educationPage } from './education.js'
+import {html,render, page} from './lib.js'
+console.log('works')
+const aboutCheck = document.querySelector('.about')
+console.log(aboutCheck)
+const root = document.querySelector('.main-content')
+console.log(root)
+page(decoration)
+// page('/',()=> console.log('ABOUT your'));
+page('/about',aboutPage)
+page('/education',educationPage)
+
+page.start()
+
+function decoration(ctx,next){
+    ctx.render = (content) => render(content,root)
+      next()
+    }
---
+++ src/education.js
@@ -0,0 +1,10 @@
+import {render, html} from './lib.js'
+
+const aboutTemplate = (homeOffers,username) => html`
+<p>Hello G.</p>`
+
+export async function educationPage(ctx) {
+    // updateLogin(null);
+  console.log('educationPage')
+
+}
