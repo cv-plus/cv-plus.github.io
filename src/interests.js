a9e0fc4594afb59fb8c733ea3aac15ccdc2b0fcc
g-Ball <georgiballabanov@gmail.com>
2022-09-18 14:18:38 +0300

    Update interest section.

--- src/interests.js
+++ src/interests.js
@@ -1,7 +1,34 @@
 import {render, html} from './lib.js'
 
 const interestsTemplate = (homeOffers,username) => html`
-<p>Hello G.</p>`
+<section class="interests">
+        <p class="hi">Hobbies</p>
+        <!-- <div class="name">
+            <p class="first-name">Georgi</p>
+            <p class="last-name">Balabanov</p>
+        </div> -->
+
+        <div class="group-skills">
+            <img src="/img/mtb.png" alt="" class="icon-skills">
+            <img src="/img/hikinh.png" alt="" class="icon-skills">
+
+            <img src="/img/paragliding.png" alt="" class="icon-skills">
+            <img src="/img/climbing.png" alt="" class="icon-skills">
+
+            <img src="/img/yoga.png" alt="" class="icon-skills">
+            <img src="/img/ski.png" alt="" class="icon-skills">
+            <img src="/img/calisthenics.png" alt="" class="icon-skills">
+
+ 
+
+        </div>
+        <!-- <div class="personal-info">
+            <p class="current-job">Currently: Technical IT Consultant</p>
+            <p class="current-job">Company: Do It Wise</p>
+            <p class="current-location">Location: Stara Zagora, Bulgaria</p>
+           
+        </div> -->
+    </section>`
 
 export async function interestsPage(ctx) {
     // updateLogin(null);
