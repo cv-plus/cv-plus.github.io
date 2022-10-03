f14ec05947295e3641035d6bdcc32530e2ba7cfa
g-Ball <georgiballabanov@gmail.com>
2022-10-03 14:49:44 +0300

    Update interest section.

--- src/interests.js
+++ src/interests.js
@@ -1,9 +1,12 @@
 import {render, html} from './lib.js'
 
-const interestsTemplate = (homeOffers,username) => html`
+const interestsTemplate = () => html`
 <section class="interests">
 <p class="hi">In my free time I enjoy to do..</p>
-
+<!-- <div class="name">
+    <p class="first-name">Georgi</p>
+    <p class="last-name">Balabanov</p>
+</div> -->
 
 <div class="group-skills">
     <img src="/img/mtb.png" alt="" class="icon-skills">
@@ -17,12 +20,64 @@ const interestsTemplate = (homeOffers,username) => html`
 
 
 </div>
+<!-- <div class="personal-info">
+    <p class="current-job">Currently: Technical IT Consultant</p>
+    <p class="current-job">Company: Do It Wise</p>
+    <p class="current-location">Location: Stara Zagora, Bulgaria</p>
+   
+</div> -->
 
-</section>`
+<p class="hi">Some of my moments..</p>
+
+<div class="glide">
+    <div class="glide__track" data-glide-el="track">
+      <ul class="glide__slides">
+      <li class="glide__slide "><img class="certs" src="img/hobby/g5.jpg" alt=""></li>
+      <li class="glide__slide"><img class="certs" src="img/hobby/b2.JPG" alt=""></li>
+      <li class="glide__slide"><img class="certs" src="img/hobby/c1.JPG" alt=""></li>
+      
+      <li class="glide__slide"><img class="certs" src="img/hobby/h2.jpg" alt=""></li>
+      <li class="glide__slide"><img class="certs" src="img/hobby/y1.JPG" alt=""></li>
+      <li class="glide__slide"><img class="certs" src="img/hobby/g3.jpg" alt=""></li>
+      <li class="glide__slide"><img class="certs" src="img/hobby/b2.jpg" alt=""></li>
+      <li class="glide__slide"><img class="certs" src="img/hobby/c2.jpg" alt=""></li>
+      <li class="glide__slide"><img class="certs" src="img/hobby/h1.jpg" alt=""></li>
+      <li class="glide__slide"><img class="certs" src="img/hobby/g7.jpg" alt=""></li>
+      <li class="glide__slide"><img class="certs" src="img/hobby/c5.jpg" alt=""></li>
+      <li class="glide__slide"><img class="certs" src="img/hobby/g6.jpg" alt=""></li>
+      <li class="glide__slide"><img class="certs" src="img/hobby/g4.jpg" alt=""></li>
+        
+        
+     
+      </ul>
+    </div>
+    <div class="glide__arrows" data-glide-el="controls">
+        <button class="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
+        <button class="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
+      </div>
+  </div>
+</section>
+`
 
 export async function interestsPage(ctx) {
     // updateLogin(null);
 
   ctx.render(interestsTemplate());
-
+  
+  const config= {
+    type:'carousel',
+    perView:3,
+    autoplay: 2000,
+    breakpoints:{
+      1024: {
+        perView: 2
+      },
+      860:{
+        perView:1
+      }
+    }
+  }
+  new Glide('.glide',config).mount()
+ 
 }
+
