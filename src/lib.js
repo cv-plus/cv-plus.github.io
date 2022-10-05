2aaf6aa5667ece214664b956b3cff2d98cc39947
g-Ball <georgiballabanov@gmail.com>
2022-10-05 15:49:54 +0300

    Library update.

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,6 @@
-import {html,render} from '../node_modules/lit-html/lit-html.js'
+import {html,render} from './lib/lit-html.js'
 // import {html,render} from './lib/lit-html.js'
-import page from '../node_modules/page/page.mjs'
+import page from './lib/page.mjs'
 
 export {
     html,render,page
